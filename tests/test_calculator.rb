f46ceaef331cc72d2dcfbba261fe035f136b49f4
Muhammed Ahmad <99442871+Muhammed-L-Ahmad@users.noreply.github.com>
2025-03-26 18:11:17 +0000

    removed empty space between lines

--- tests/test_calculator.rb
+++ tests/test_calculator.rb
@@ -10,8 +10,6 @@ class TestCalculator < Minitest::Test
     assert_equal 4, calculator.add(2, 2), "addition method failed"
   end
 
-
-
   def test_subtraction
     calculator = Calculator.new
     assert_equal 0, calculator.subtract(2, 2), "subtraction method failed"
