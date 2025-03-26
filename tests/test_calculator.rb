29d750db3c594e3b265366fa7489ffef269f19ba
Muhammed Ahmad <99442871+Muhammed-L-Ahmad@users.noreply.github.com>
2025-03-26 17:02:07 +0000

    added subtraction test

--- tests/test_calculator.rb
+++ tests/test_calculator.rb
@@ -7,3 +7,9 @@ class TestCalculator < Minitest::Test
     assert_equal 4, calculator.add(2, 2), "addition method failed"
   end
 end
+
+  def test_subtraction
+    calculator = Calculator.new
+    assert_equal 0, calculator.subtract(2, 2), "subtraction method failed"
+  end
+end
