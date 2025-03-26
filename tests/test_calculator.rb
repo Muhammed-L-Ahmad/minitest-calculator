b54961976ab33c10e8e6589fc5ecc98e0d82b7fc
Muhammed Ahmad <99442871+Muhammed-L-Ahmad@users.noreply.github.com>
2025-03-26 17:17:36 +0000

    correcting new syntax error by replacing "end"s

--- tests/test_calculator.rb
+++ tests/test_calculator.rb
@@ -6,9 +6,11 @@ class TestCalculator < Minitest::Test
     calculator = Calculator.new
     assert_equal 4, calculator.add(2, 2), "addition method failed"
   end
+end
 
 
   def test_subtraction
     calculator = Calculator.new
     assert_equal 0, calculator.subtract(2, 2), "subtraction method failed"
   end
+end
