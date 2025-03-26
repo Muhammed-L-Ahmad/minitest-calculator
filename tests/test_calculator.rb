0da0447a0321cae847bd3c5bb22db5ebcbcd0d93
Muhammed Ahmad <99442871+Muhammed-L-Ahmad@users.noreply.github.com>
2025-03-26 16:53:09 +0000

    TestCalculator class created

--- tests/test_calculator.rb
+++ tests/test_calculator.rb
@@ -1,2 +1,9 @@
 require 'minitest/autorun'
 require './calculator'
+
+class TestCalculator < Minitest::Test
+  def test_addition
+    calculator = Calculator.new
+    assert_equal 4, calculator.add(2, 2), "addition method failed"
+  end
+end
