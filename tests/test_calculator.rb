63fd167da31db0354dcfe88c12f35514ba9be8fc
Muhammed Ahmad <99442871+Muhammed-L-Ahmad@users.noreply.github.com>
2025-03-26 18:36:53 +0000

    changed variables into instance variables

--- tests/test_calculator.rb
+++ tests/test_calculator.rb
@@ -4,14 +4,16 @@ require './calculator'
 class TestCalculator < Minitest::Test
   def setup
     @calculator = Calculator.new
+    @sum = @calculator.add(2, 2)
+    @difference = @calculator.subtract(2, 2)
   end
 
   def test_addition
-    assert_equal 4, calculator.add(2, 2), "addition method failed"
+    assert_equal 4, @sum, "addition method failed"
   end
 
   def test_subtraction
     calculator = Calculator.new
-    assert_equal 0, calculator.subtract(2, 2), "subtraction method failed"
+    assert_equal 0, @difference, "subtraction method failed"
   end
 end
